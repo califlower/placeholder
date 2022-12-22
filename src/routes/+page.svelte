612038cff06743d237802724060b32d2e83a222d
Calin Gilan <cgilan@twilio.com>
2022-12-22 11:52:28 -0500

    Redesign Homepage partial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,32 @@
-<div class="flex flex-1 flex-col bg-gradient-to-b from-purple-500 to-pink-500">
+<div class="homepage-gradient flex flex-1 flex-col">
 	<div class="m-6 flex h-full flex-col items-center justify-center">
-		<div class="w-80 md:w-fit">
-			<h2
-				class="mb-3 text-4xl font-bold text-slate-200 md:text-5xl md:font-extrabold md:leading-tight"
+		<div class="prose mx-auto">
+			<h1
+				class=" mb-3 inline text-4xl font-bold text-slate-200 md:text-5xl md:font-extrabold md:leading-tight"
 			>
-				Discover the best card for you
-			</h2>
-			<div class="h-2" />
-			<h1 class="text-5xl font-semibold text-slate-200">
-				on <p class="inline text-blue-100 underline">pointlift.ai</p>
+				Discover the best card for you on
+				<p class="inline text-blue-100 underline">pointlift.ai</p>
 			</h1>
+
 			<div class="h-4" />
-			<p class="mb-6 text-lg text-slate-200 md:text-xl md:leading-normal">
-				Compare potential card card rewards from across the market using personalized transaction
-				data
-			</p>
+			<div class="prose mx-auto">
+				<p class="mb-6 text-lg text-slate-200 md:text-xl md:leading-normal">
+					Compare potential card card rewards from across the market using personalized transaction
+					data
+				</p>
+			</div>
 			<div class="h-12" />
 			<div class="flex items-center justify-center">
-				<button class="rounded-md bg-blue-600 py-2 px-4 text-slate-200"
-					>Connect your current card</button
-				>
+				<button class="glass btn">Connect your current card</button>
 			</div>
 			<div class="h-4" />
-			<div class="flex justify-center">
+			<div class="flex flex-col items-center justify-center space-y-3">
 				<p class="text-slate-200">Or view an estimate using your zip code!</p>
+				<input
+					type="text"
+					placeholder="Zipcode"
+					class="input-bordered input-primary input w-full max-w-xs"
+				/>
 			</div>
 		</div>
 	</div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,135 +1,22 @@
-<div class="flex-1">
-	<div class="px-6 pt-6 2xl:container">
-		<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-			<div class="md:col-span-2 lg:col-span-1">
-				<div class="h-full space-y-6 rounded-xl border border-gray-200 bg-white py-8 px-6">
-					<div>
-						<h5 class="text-center text-xl text-gray-600">Overall Spend</h5>
-						<div class="mt-2 flex justify-center gap-4">
-							<h3 class="text-3xl font-bold text-gray-700">$23,988</h3>
-							<div class="flex items-end gap-1 text-green-500">
-								<svg class="w-3" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path d="M6.00001 0L12 8H-3.05176e-05L6.00001 0Z" fill="currentColor" />
-								</svg>
-								<span>2%</span>
-							</div>
-						</div>
-						<span class="block text-center text-gray-500">Compared to last week $13,988</span>
-					</div>
-					<table class="w-full text-gray-600">
-						<tbody>
-							<tr>
-								<td class="py-2">Tailored ui</td>
-								<td class="text-gray-500">896</td>
-								<td>
-									<svg
-										class="ml-auto w-16"
-										viewBox="0 0 68 21"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<rect opacity="0.4" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="19" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="35" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="51" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<path
-											d="M0 7C8.62687 7 7.61194 16 17.7612 16C27.9104 16 25.3731 9 34 9C42.6269 9 44.5157 5 51.2537 5C57.7936 5 59.3731 14.5 68 14.5"
-											stroke="url(#paint0_linear_622:13631)"
-											stroke-width="2"
-											stroke-linejoin="round"
-										/>
-										<defs>
-											<linearGradient
-												id="paint0_linear_622:13631"
-												x1="68"
-												y1="7.74997"
-												x2="1.69701"
-												y2="8.10029"
-												gradientUnits="userSpaceOnUse"
-											>
-												<stop stop-color="#E323FF" />
-												<stop offset="1" stop-color="#7517F8" />
-											</linearGradient>
-										</defs>
-									</svg>
-								</td>
-							</tr>
-							<tr>
-								<td class="py-2">Customize</td>
-								<td class="text-gray-500">1200</td>
-								<td>
-									<svg
-										class="ml-auto w-16"
-										viewBox="0 0 68 21"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<rect opacity="0.4" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="19" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="35" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="51" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<path
-											d="M0 12.929C8.69077 12.929 7.66833 9.47584 17.8928 9.47584C28.1172 9.47584 25.5611 15.9524 34.2519 15.9524C42.9426 15.9524 44.8455 10.929 51.6334 10.929C58.2217 10.929 59.3092 5 68 5"
-											stroke="url(#paint0_linear_622:13640)"
-											stroke-width="2"
-											stroke-linejoin="round"
-										/>
-										<defs>
-											<linearGradient
-												id="paint0_linear_622:13640"
-												x1="34"
-												y1="5"
-												x2="34"
-												y2="15.9524"
-												gradientUnits="userSpaceOnUse"
-											>
-												<stop stop-color="#8AFF6C" />
-												<stop offset="1" stop-color="#02C751" />
-											</linearGradient>
-										</defs>
-									</svg>
-								</td>
-							</tr>
-							<tr>
-								<td class="py-2">Other</td>
-								<td class="text-gray-500">12</td>
-								<td>
-									<svg
-										class="ml-auto w-16"
-										viewBox="0 0 68 21"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<rect opacity="0.4" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="19" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="35" width="14" height="21" rx="1" fill="#e4e4f2" />
-										<rect opacity="0.4" x="51" width="17" height="21" rx="1" fill="#e4e4f2" />
-										<path
-											d="M0 6C8.62687 6 6.85075 12.75 17 12.75C27.1493 12.75 25.3731 9 34 9C42.6269 9 42.262 13.875 49 13.875C55.5398 13.875 58.3731 6 67 6"
-											stroke="url(#paint0_linear_622:13649)"
-											stroke-width="2"
-											stroke-linejoin="round"
-										/>
-										<defs>
-											<linearGradient
-												id="paint0_linear_622:13649"
-												x1="67"
-												y1="7.96873"
-												x2="1.67368"
-												y2="8.44377"
-												gradientUnits="userSpaceOnUse"
-											>
-												<stop stop-color="#FFD422" />
-												<stop offset="1" stop-color="#FF7D05" />
-											</linearGradient>
-										</defs>
-									</svg>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+<div class="flex flex-1 items-center justify-center    ">
+	<div>
+		<div class="mb-24 text-center">
+			<h1 class="mb-4 text-xl font-bold leading-tight text-gray-900 md:text-2xl">
+				No Cards Connected!
+			</h1>
+			<p class="mb-5 text-base text-gray-600 md:text-lg">
+				You'll need to connect a credit card in order to see recommendations!
+			</p>
+			<div class="h-4" />
+			<div>
+				<ul class="steps">
+					<li class="step-primary step mx-12">Connect</li>
+					<li class="step mx-2">Analyze!</li>
+					<li class="step">Done!</li>
+				</ul>
 			</div>
+			<div class="h-12" />
+			<button class="btn">Connect Card</button>
 		</div>
 	</div>
 </div>
