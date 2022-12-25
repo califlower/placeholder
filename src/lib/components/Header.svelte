4fd13056faa458aedad44f61e1e9a05b9fa8c7ef
Calin Gilan <cgilan@twilio.com>
2022-12-24 21:26:56 -0500

    New Home Page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,51 +1,77 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	type Tab = {
+		displayName: string;
+		ref: string;
+		queryParam: string;
+	};
+
+	const tabs: Tab[] = [
+		{ displayName: 'Stats', ref: '/dashboard/stats', queryParam: 'stats' },
+		{ displayName: 'Transactions', ref: '/dashboard/transactions', queryParam: 'transactions' },
+		{ displayName: 'Cards', ref: '/dashboard/cards', queryParam: 'transaction' }
+	];
+
+	const sideBar: boolean = false;
 </script>
 
 <header>
 	<div class="mx-auto flex max-w-7xl items-center justify-between p-4">
-		<div class="flex items-center space-x-2">
-			<button
-				class="flex appearance-none p-1 {$page.url.pathname === '/'
-					? 'text-white'
-					: 'text-slate-700'} md:hidden"
-			>
-				<svg class="h-6 w-6" fill="currentColor" viewBox="0 0 256 256">
-					<line
-						x1="40"
-						y1="128"
-						x2="216"
-						y2="128"
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="24"
-					/>
-					<line
-						x1="40"
-						y1="64"
-						x2="216"
-						y2="64"
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="24"
-					/>
-					<line
-						x1="40"
-						y1="192"
-						x2="216"
-						y2="192"
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="24"
-					/>
-				</svg>
-			</button>
+		<div class="flex items-center">
+			<div class="dropdown dropdown-right">
+				<button
+					class="flex appearance-none p-1 {$page.url.pathname === '/'
+						? 'text-white'
+						: 'text-slate-700'} md:hidden"
+				>
+					<svg class="h-6 w-6 hover:opacity-60" fill="currentColor" viewBox="0 0 256 256">
+						<line
+							x1="40"
+							y1="128"
+							x2="216"
+							y2="128"
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="24"
+						/>
+						<line
+							x1="40"
+							y1="64"
+							x2="216"
+							y2="64"
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="24"
+						/>
+						<line
+							x1="40"
+							y1="192"
+							x2="216"
+							y2="192"
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="24"
+						/>
+					</svg>
+					<ul
+						tabindex="-1"
+						class="dropdown-content menu rounded-box absolute mt-3 w-52 bg-base-100 p-2 text-slate-900 shadow-lg"
+					>
+						<li>
+							<a href="/dashboard/stats" class="justify-between"> Dashboard </a>
+						</li>
+						<li><a href="/how-it-works">How It Works</a></li>
+						<li><a href="/contact">Contact</a></li>
+						<li><a href="/about">About</a></li>
+					</ul>
+				</button>
+			</div>
 			<a href="/">
 				<div class="align-center flex items-center space-x-2 ">
 					<img class="h-12 w-12" src="/logo.png" alt="pointlift company logo" />
@@ -86,7 +112,6 @@
 				class="rounded  py-2  transition hover:underline hover:opacity-60">About</a
 			>
 		</nav>
-
 		<nav
 			class="flex items-center space-x-1 text-base font-medium {$page.url.pathname === '/'
 				? 'text-white'
